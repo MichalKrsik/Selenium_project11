7b7add9099c4fade49f672f1d1e8a28587f6688f
petra.heczkova <petra.heczkova@homecredit.cz>
2023-04-09 00:35:50 +0200

    Initial commit

---
+++ test_data/test_data.py
@@ -0,0 +1,5 @@
+class Addresses:
+
+    test_addresses_data = [{"first_name": "Michal", "last_name": "Krsik", "zip_code": "62100"},
+                           {"first_name": "Petra", "last_name": "Heczkova", "zip_code": "62101"},
+                           {"first_name": "Jan", "last_name": "Brazdil", "zip_code": "62102"}]
